4fa88b5c95565d085eaf355712c5a8acc59ac356
Rama143g <ramiro-1593@hotmail.com>
2023-10-02 21:09:57 -0300

    pack entidad
    class paciente y comida

--- src/Entidades/Comida.java
+++ src/Entidades/Comida.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Entidades;
 
-/**
- *
- * @author ramir
- */
+
 public class Comida {
+    
+    private int idComida;
+    private String nombre;
+    private String detalle;
+    private double calXgr;                             
+
+    public int getIdComida() {
+        return idComida;
+    }
+
+    public void setIdComida(int idComida) {
+        this.idComida = idComida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public double getCalXgr() {
+        return calXgr;
+    }
+
+    public void setCalXgr(int calXgr) {
+        this.calXgr = calXgr;
+    }
+
+    public Comida(String nombre, String detalle, int calXgr) {
+        this.nombre = nombre;
+        this.detalle = detalle;
+        this.calXgr = calXgr;
+    }
+
+    public Comida() {
+    }
+
+    public Comida(int idComida, String nombre, String detalle, int calXgr) {
+        this.idComida = idComida;
+        this.nombre = nombre;
+        this.detalle = detalle;
+        this.calXgr = calXgr;
+    }
+    
     
 }
--- src/Entidades/Paciente.java
+++ src/Entidades/Paciente.java
@@ -1,14 +1,116 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Entidades;
 
-/**
- *
- * @author ramir
- */
+
+
+
 public class Paciente {
+    
+    private int idPaciente;
+    private int DNI;
+    private String apellido;
+    private String nombre;
+    private String domicilio;
+    private int celular;
+    private double pesoActual;
+    private double pesoDeseado;
+    private boolean estado;
+
+    public Paciente(int DNI, String apellido, String nombre, String domicilio, int celular, double pesoActual, double pesoDeseado,boolean estado) {
+        this.DNI = DNI;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.celular = celular;
+        this.pesoActual = pesoActual;
+        this.pesoDeseado = pesoDeseado;
+        this.estado = estado;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public Paciente() {
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public double getPesoActual() {
+        return pesoActual;
+    }
+
+    public void setPesoActual(double pesoActual) {
+        this.pesoActual = pesoActual;
+    }
+
+    public double getPesoDeseado() {
+        return pesoDeseado;
+    }
+
+    public void setPesoDeseado(double pesoDeseado) {
+        this.pesoDeseado = pesoDeseado;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return "Paciente{" + "idPaciente=" + idPaciente + ", apellido=" + apellido + ", nombre=" + nombre + '}';
+            
+    }
+    
+    
+    
     
 }
