ba23c1aeea816845a9e009cc0117f9af74fe77b2
Rama143g <ramiro-1593@hotmail.com>
2023-10-10 19:32:12 -0300

    vista pacientes + imagenes
    imagenes x 2

--- src/Views/Nutricionista_App.java
+++ src/Views/Nutricionista_App.java
@@ -27,11 +27,6 @@ public class Nutricionista_App extends javax.swing.JFrame {
         SoyUnMenu = new javax.swing.JMenuBar();
         JMpaciente = new javax.swing.JMenu();
         infoPaciente = new javax.swing.JMenuItem();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem5 = new javax.swing.JMenuItem();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
         JMmenú = new javax.swing.JMenu();
         infoMenu = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
@@ -41,8 +36,12 @@ public class Nutricionista_App extends javax.swing.JFrame {
         jMenuItem8 = new javax.swing.JMenuItem();
         jMenuItem9 = new javax.swing.JMenuItem();
         jMenuItem10 = new javax.swing.JMenuItem();
-        jMenu3 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         jMenuItem11.setText("jMenuItem11");
 
@@ -54,14 +53,14 @@ public class Nutricionista_App extends javax.swing.JFrame {
         DesktopMain.setLayout(DesktopMainLayout);
         DesktopMainLayout.setHorizontalGroup(
             DesktopMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 528, Short.MAX_VALUE)
         );
         DesktopMainLayout.setVerticalGroup(
             DesktopMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 265, Short.MAX_VALUE)
+            .addGap(0, 411, Short.MAX_VALUE)
         );
 
-        JMpaciente.setText("Paciente");
+        JMpaciente.setText("Pacientes");
 
         infoPaciente.setText("Paciente Info");
         infoPaciente.addActionListener(new java.awt.event.ActionListener() {
@@ -73,23 +72,7 @@ public class Nutricionista_App extends javax.swing.JFrame {
 
         SoyUnMenu.add(JMpaciente);
 
-        jMenu1.setText("Dieta");
-
-        jMenuItem5.setText("Dieta...");
-        jMenu1.add(jMenuItem5);
-
-        jMenuItem1.setText("Dieta Nueva");
-        jMenu1.add(jMenuItem1);
-
-        jMenuItem2.setText("Modificar Dieta");
-        jMenu1.add(jMenuItem2);
-
-        jMenuItem3.setText("Eliminar Dieta");
-        jMenu1.add(jMenuItem3);
-
-        SoyUnMenu.add(jMenu1);
-
-        JMmenú.setText("Menú");
+        JMmenú.setText("Comidas");
 
         infoMenu.setText("Menú Info");
         infoMenu.addActionListener(new java.awt.event.ActionListener() {
@@ -123,11 +106,24 @@ public class Nutricionista_App extends javax.swing.JFrame {
 
         SoyUnMenu.add(jMenu2);
 
-        jMenu3.setText("HELP!!!!!!!!!!");
-        SoyUnMenu.add(jMenu3);
-
         jMenu4.setText("Salir");
         SoyUnMenu.add(jMenu4);
+
+        jMenu1.setText("Dieta");
+
+        jMenuItem5.setText("Dieta...");
+        jMenu1.add(jMenuItem5);
+
+        jMenuItem1.setText("Dieta Nueva");
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Modificar Dieta");
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("Eliminar Dieta");
+        jMenu1.add(jMenuItem3);
+
+        SoyUnMenu.add(jMenu1);
 
         setJMenuBar(SoyUnMenu);
 
@@ -199,7 +195,6 @@ public class Nutricionista_App extends javax.swing.JFrame {
     private javax.swing.JMenuItem infoPaciente;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem10;
